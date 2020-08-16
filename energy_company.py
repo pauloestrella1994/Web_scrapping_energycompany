1c70e1ec2decc7a947e2e60c9125f75b6a776fc6
Paulo Netto <pauloestrella1994@gmail.com>
2020-08-16 12:55:54 -0300

    Login_data wip

--- energy_company.py
+++ energy_company.py
@@ -6,6 +6,8 @@ class EnergyCompany:
         self.driver = driver
         self.url = 'https://www.energycompany.com.br/'
         self.class_login = "jss1"
+        self.class_login_data_email = '/html/body/div/div/div/div/form/div/div[2]/div/div[1]/div[1]/label'
+        self.class_login_data_password = '/html/body/div/div/div/div/form/div/div[2]/div/div[1]/div[2]/label'
 
     def navigate(self):
         self.driver.get(self.url)
@@ -14,10 +16,18 @@ class EnergyCompany:
         self.driver.find_element_by_class_name(
             self.class_login).click()
 
+    def login_data(self):
+        email = input('Insert your email: ')
+        password = input('Insert your password: ')
+        self.driver.find_element_by_xpath(self.class_login_data_email).send_keys(email)
+        self.driver.find_element_by_xpath(self.class_login_data_password).send_keys(password)
+
+
 ##----------------Instancias-------------##
 
 browser = webdriver.Firefox()
 start = EnergyCompany(browser)
 start.navigate()
 start.login()
+start.login_data()
 

8d3aedf7345fa67ce0a3f6bc39974bd62e53f14e
Paulo Netto <pauloestrella1994@gmail.com>
2020-09-06 12:05:04 -0300

    Login

--- energy_company.py
+++ energy_company.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
-import requests
-
 
 class EnergyCompany:
 
@@ -13,17 +11,15 @@ class EnergyCompany:
         self.driver.get(self.url_login)
 
     def login_data(self):
-        data = {
-            "username": "pauloestrella1994@gmail.com",
-            "password": "Paulo1994",
-            "grant_type": "password"
-        }
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:80.0) Gecko/20100101 Firefox/80.0"
-        }
-        with requests.Session() as s:
-            request_login = s.post(self.url_login, data=data, headers=headers)
-            print(request_login.content)
+        username_path = self.driver.find_element_by_xpath('/html/body/div/div/div/div/form/div/div[2]/div/div[1]/div[1]/div/input')
+        password_path = self.driver.find_element_by_xpath(('/html/body/div/div/div/div/form/div/div[2]/div/div[1]/div[2]/div/input'))
+        button = self.driver.find_element_by_xpath('/html/body/div/div/div/div/form/div/div[2]/div/div[3]/div/button')
+
+        username_data = str(input('Email: '))
+        password_data = str(input('Password: '))
+        username_path.send_keys(username_data)
+        password_path.send_keys(password_data)
+        button.click()
 
 ##----------------Instancias-------------##
 
